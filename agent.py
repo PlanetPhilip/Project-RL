208c1b3895ba328c71bfdb980aa1703e65e8209b
PlanetPhilip <97726581+PlanetPhilip@users.noreply.github.com>
2025-01-12 12:49:00 +0100

    Discretized Action Space

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import subprocess
 
 
 class QAgent:
@@ -6,24 +7,27 @@ class QAgent:
     def __init__(self, env, discount_rate=0.95, bin_size=20):
         self.env = env
         self.discount_rate = discount_rate
-        self.action_space = self.env.continuous_action_space
 
-        # Create bins
+        # Discretize Action Space
+        self.action_space = np.linspace(env.continuous_action_space.low, env.continuous_action_space.high, 3)
+
+        # Discretize State Space
         self.bin_storage_level = np.linspace(env.storage_level, 170, bin_size)
-        self.bin_price = np.linspace(np.min(env.price_values), np.max(env.price_values, bin_size))
+        self.bin_price = np.linspace(np.min(env.price_values), np.max(env.price_values), bin_size)
         self.bin_hour = np.linspace(env.hour, 24, bin_size)
         self.bin_day = np.linspace(env.day, len(env.price_values), bin_size)
         self.bins = [self.bin_storage_level, self.bin_price, self.bin_hour, self.bin_day]
+        # TODO: maybe a dictionary is better?
 
         # Construct Q-table
-        discrete_state_space = (bin_size - 1,) * len(self.bins)  # TODO: do we need to do -1 ?
-        discrete_action_space = (self.action_space,)  # TODO: discretize action space
-        self.Qtable = np.zeros(discrete_state_space + discrete_action_space)
+        action_space_size = len(self.action_space)
+        state_space_size = (bin_size - 1) ** len(self.bins)  # TODO: do we need to do -1 ?
+        self.Qtable = np.zeros((state_space_size, action_space_size))
 
 
-    def discretize_state(self):
-        # TODO: write function
-        return state
+    def discretize_state(self, state):
+        discretized_state = [np.digitize(state[i], self.bins[i]) for i in range(len(state))]
+        return discretized_state
 
     def act(self, state):
         # TODO: write function
@@ -32,27 +36,4 @@ class QAgent:
 
 
 if __name__ == '__main__':
-
-    # Create environment
-    from env import DataCenterEnv
-
-    environment = DataCenterEnv('./Data/train.xlsx')
-
-    # Initialize agent
-    agent = QAgent(environment)
-
-    # Run agent
-    aggregate_reward = 0
-    terminated = False
-    state = environment.observation()
-    while not terminated:
-        action = agent.act(state)
-        next_state, reward, terminated = environment.step(
-            action)  # next_state is given as: [storage_level, price, hour, day]
-        state = next_state
-        aggregate_reward += reward
-
-        print("Action:", action, )
-        print("Next state:", next_state)
-        print("Reward:", reward)
-    print('Total reward:', aggregate_reward)
+    subprocess.run(['python', 'main.py'])
