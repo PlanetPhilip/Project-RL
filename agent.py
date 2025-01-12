3bfbdc5b30e9b55d799dcd9d947236a60556e7cd
PlanetPhilip <97726581+PlanetPhilip@users.noreply.github.com>
2025-01-12 16:44:59 +0100

    Added act function

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import gym
 import numpy as np
 import subprocess
 
@@ -9,7 +10,7 @@ class QAgent:
         self.discount_rate = discount_rate
 
         # Discretize Action Space
-        self.action_space = np.linspace(env.continuous_action_space.low, env.continuous_action_space.high, 3)
+        self.action_space = gym.spaces.Discrete(3)
 
         # Discretize State Space
         self.bin_storage_level = np.linspace(env.storage_level, 170, bin_size)
@@ -17,21 +18,35 @@ class QAgent:
         self.bin_hour = np.linspace(env.hour, 24, bin_size)
         self.bin_day = np.linspace(env.day, len(env.price_values), bin_size)
         self.bins = [self.bin_storage_level, self.bin_price, self.bin_hour, self.bin_day]
-        # TODO: maybe a dictionary is better?
 
         # Construct Q-table
-        action_space_size = len(self.action_space)
-        state_space_size = (bin_size - 1) ** len(self.bins)  # TODO: do we need to do -1 ?
-        self.Qtable = np.zeros((state_space_size, action_space_size))
-
+        action_space_size = (self.action_space.n,)
+        state_space_size = (bin_size,) * len(self.bins)  # TODO: do we need to do -1 ?
+        self.Qtable = np.zeros(state_space_size + action_space_size)
 
     def discretize_state(self, state):
         discretized_state = [np.digitize(state[i], self.bins[i]) - 1 for i in range(len(state))]
         return discretized_state
 
-    def act(self, state):
-        # TODO: write function
-        action = np.random.uniform(-1, 1)
+    def act(self, state, epsilon=0.05, learning_rate=0.1):
+        """Chooses an action based on epsilon-greedy policy, and updates the Qtable"""
+
+        # Discretize State
+        state = self.discretize_state(state)
+
+        # Picks Action based on Epsilon Greedy
+        if np.random.uniform(0, 1) < epsilon:
+            action = self.action_space.sample()
+        else:
+            action = np.argmax(self.Qtable[tuple(state), :])
+
+        # Update Qtable
+        next_state, reward, _ = self.env.step(action)
+        next_state = self.discretize_state(next_state)
+        Q_target = (reward + self.discount_rate * np.max(self.Qtable[tuple(next_state)]))
+        delta = learning_rate * (Q_target - self.Qtable[tuple(state) + (action,)])
+        self.Qtable[tuple(state) + (action,)] = self.Qtable[tuple(state) + (action,)] + delta
+
         return action
 
 
